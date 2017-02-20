39bdd6026450f03685bae662a331e9847f4dc73f
root <root@beaglebone.localdomain>
2017-02-20 20:53:05 +0000

    Trying to fix motion sensor

--- Motion/src/motionRecognition.py
+++ Motion/src/motionRecognition.py
@@ -2,18 +2,17 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
  
 #create a variable called PIR, which refers to the P8_11 pin
-PIR = "P8_11"
-led = "P8_13";
+PIR = "P8_19"
  
 #initialize the pin as an INPUT
 GPIO.setup(PIR, GPIO.IN)
-GPIO.setup(led, GPIO.OUT)
-GPIO.add_event_detect(PIR, GPIO.RISING)
 
+#loop forever
+print "Start"
 while True:
     #sends an email when motion has been detected
-    if GPIO.event_detected(PIR):
-        GPIO.output(led, GPIO.HIGH) 
+    if GPIO.input(PIR):
+        print("Motion Detected")
     else:
-    	GPIO.output(led, GPIO.LOW)
-    time.sleep(0.05) #loop every 50 miliseconds to not overburden the CPU
+        print("No Motion Detected")
+    time.sleep(2) 
