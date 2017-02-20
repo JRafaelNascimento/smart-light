480a50b47ad2a873842b444cdf2691e032ad6115
root <root@beaglebone.localdomain>
2017-02-20 21:17:43 +0000

    Adding code of LED on motion sensor

--- Motion/src/motionRecognition.py
+++ Motion/src/motionRecognition.py
@@ -3,16 +3,19 @@ import time
  
 #create a variable called PIR, which refers to the P8_11 pin
 PIR = "P8_19"
+LED = "P8_17"
  
 #initialize the pin as an INPUT
 GPIO.setup(PIR, GPIO.IN)
+GPIO.setup(LED, GPIO.OUT)
 
 #loop forever
-print "Start"
 while True:
     #sends an email when motion has been detected
     if GPIO.input(PIR):
+        GPIO.output(LED, GPIO.HIGH)
         print("Motion Detected")
     else:
+        GPIO.output(LED, GPIO.LOW)
         print("No Motion Detected")
     time.sleep(2) 
